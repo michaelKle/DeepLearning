1a2c5d6c7e17a4d71c227feacda579a5d006902e
michaelKle <3406283+michaelKle@users.noreply.github.com>
2023-12-18 16:51:01 +0100

    Also add torch

--- Math/math_transpose.py
+++ Math/math_transpose.py
@@ -1,6 +1,8 @@
 import numpy as np
 import torch
 
+print('##### Vector with numpy')
+
 nv = np.array([[1,2,3,4]])
 print(nv), print(' ')
 
@@ -10,7 +12,7 @@ nvT = nv.T
 print (nvT.T), print(' ')
 
 
-print('#####')
+print('##### Matrix with numpy')
 
 nM = np.array([ [1,2,3,4], [5,6,7,8]])
 print(nM), print (' ')
@@ -18,3 +20,25 @@ print(nM), print (' ')
 print(nM.T), print (' ')
 
 print(nM.T.T), print (' ')
+
+print('##### Vector with torch')
+
+tv = torch.tensor([[1,2,3,4]])
+print(tv), print(' ')
+print (tv.T), print(' ')
+print (tv.T.T), print(' ')
+
+
+print('##### Matrix with torch')
+tM = torch.tensor([ [1,2,3,4], [5,6,7,8]])
+print(tM), print (' ')
+print(tM.T), print (' ')
+print(tM.T.T), print (' ')
+
+
+
+print('##### Types')
+print(f'nv is of type {type(nv)}')
+print(f'nM is of type {type(nM)}')
+print(f'tv is of type {type(tv)}')
+print(f'tM is of type {type(tM)}')
