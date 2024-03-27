32eae0fb55e84ed7d63650a167e66da4a0371bed
michaelKle <3406283+michaelKle@users.noreply.github.com>
2024-03-27 14:01:40 +0100

    Product rule and chain rule of derivatives

--- Math/math_derivative.py
+++ Math/math_derivative.py
@@ -25,7 +25,7 @@ p = symplot(relu, (x,-4,4), label='ReLU', show=False, line_color='blue')
 p.extend(symplot(sigmoid, (x,-4,4), label='Sigmoid', show=False, line_color='red'))
 p.legend = True
 p.title = 'The functions'
-p.show()
+#p.show()
 plt.savefig('sigrelu')
 
 # now their derivatives
@@ -33,5 +33,30 @@ p = symplot(sym.diff(relu), (x,-4,4), label='df(ReLU)', show=False, line_color='
 p.extend(symplot(sym.diff(sigmoid), (x,-4,4), label='df(Sigmoid)', show=False, line_color='red'))
 p.legend = True
 p.title = 'The functions'
-p.show()
+#p.show()
 plt.savefig('dfsigrelu')
+
+
+## example of derivative of product of functions
+
+gx = 4*x**3 - 3*x**4
+
+df = sym.diff(fx)
+dg = sym.diff(gx)
+
+print(f"f={fx} f'={df}")
+print(f"g={gx} g'={dg}")
+
+mult = fx*gx
+dmult = sym.diff(mult)
+print(f"f*g={mult} (f*g)`={dmult}")
+
+## example of chain rule
+gx = x**2 + 4*x**3
+fx = (gx)**5
+
+df = sym.diff(fx)
+dg = sym.diff(gx)
+
+print(f"g={gx} g'={dg}")
+print(f"f={fx} f'={df}")
